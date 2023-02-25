e4b765d206569b39f9c6bb75d70735d20566b661
Aral Roca <aral-rg@hotmail.com>
2023-02-25 12:07:12 +0100

    remove autogenerate file

--- packages/next-load/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    namespace globalThis {
-        var __NEXT_LOAD__: any;
-    }
-}
-export declare function consume<T>(): T;
-export declare function _useHydrate(): void;
-export default consume;
